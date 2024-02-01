7a38131d65dd07c54a0df9d5188ff158a480b784
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-02-01 13:11:34 +0100

    change structure of data

--- src/data/data.js
+++ src/data/data.js
@@ -29,48 +29,48 @@ export const data_of_questions =
     ],
     questions:[
         {
-            id: 1,
+            number: 4,
             question: {
                 text: "Wie viel und wann?",
                 points_for_right_answer: 10,
             },
             answers:[
                 {
-                    id: 1,
+                    number: 1,
                     text: "100 und Heute",
                     richtich: true,
                 },
                 {
-                    id: 2,
+                    number: 2,
                     text: "10 und Zukunft",
                     richtich: false
                 },
                 {
-                    id: 3,
+                    number: 3,    
                     text: "50 und Gestern",
                     richtich: false
                 }
             ]
         },
         {
-            id: 1,
+            number: 6,
             question: {
                 text: "Wo und wie?",
                 points_for_right_answer: 15,
             },
             answers:[
                 {
-                    id: 1,
+                    number: 1,
                     text: "Stadt und mit Bus",
                     richtich: false,
                 },
                 {
-                    id: 2,
+                    number: 2,
                     text: "Dorf und mit Traktor",
                     richtich: true,
                 },
                 {
-                    id: 3,
+                    number: 3,
                     antwort: "Meer und mit Auto",
                     text: false
                 }
