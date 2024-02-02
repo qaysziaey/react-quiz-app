cb82b6c411d40694028e20360d106f9eec2fa658
Qais Ziaey <qziaey@gmail.com>
2024-02-02 15:11:28 +0100

    work on Werbung UI Design and changes in App.css color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 import { Main_page } from "./components/Main_page.jsx";
 import { Question } from "./components/Question.jsx";
