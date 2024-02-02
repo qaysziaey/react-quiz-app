be8c0bdfcd9e59f8e30389bace39bae2d9a98572
Qais Ziaey <qziaey@gmail.com>
2024-02-02 10:27:58 +0100

    create Question box, change buttons ui

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,51 @@
-import './Question.module.css'
-import { data_of_questions } from '../data/data.js'
+import styles from "./Question.module.css";
+import { data_of_questions } from "../data/data.js";
+import { useState } from "react";
 
-export function Question(props){
-    return (
-        <>
-            <h1>Answer the questions</h1>
-        </>
-    )
+export function Question(props) {
+  const [getData, setGetData] = useState(data_of_questions);
+
+  //   function handleQuestions() {
+  //     getData.map((data) => {
+  //       console.log(data);
+  //     });
+  //   }
+
+  return (
+    <>
+      <div className={styles["questions-container"]}>
+        <h1>QUIZ QUESTIONS</h1>
+        <h2 className={styles["rounds-counter"]}>5/8</h2>
+        <div className={styles["questions"]}>
+          <form>
+            {data_of_questions.prizes_by_points.map((prize, index) => {})}
+            <p className={styles["question-text"]}>
+              Wie selektiert man ein Element mit id=demo?
+            </p>
+            <label>
+              <input name="a" type="checkbox" value="option1" checked />
+              #demo
+            </label>
+
+            <label>
+              <input name="b" type="checkbox" value="option1" />
+              .demo
+            </label>
+            <label>
+              <input name="c" type="checkbox" value="option1" />
+              Demo
+            </label>
+            <label>
+              <input name="d" type="checkbox" value="option1" />
+              *demo
+            </label>
+          </form>
+        </div>
+        <div className={styles["buttons-contanier"]}>
+          <button className={styles.button}>Back</button>
+          <button className={styles.button}>Next</button>
+        </div>
+      </div>
+    </>
+  );
 }
