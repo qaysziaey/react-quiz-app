48179efa15b6cb93cc5c6baa590426d3cac01c49
Raghd Alabdullah <alabdallahraghad28@gmail.com>
2024-02-05 11:08:36 +0100

    Css Hinzufügt

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,21 +3,22 @@ import styles from "./Results.module.css";
 
 export function Results(props) {
   const [totalScore, setTotalScore] = useState(0);
-  const [message, setMessage] = useState({ text: "", backgroundColor: "" });
-
-  const maxScore = 8;
+  const [message, setMessage] = useState({ text: "", color: "" });
 
   useEffect(() => {
-    const score = props.userAnswers.reduce(
-      (accumulator, currentValue) => accumulator + (currentValue ? 1 : 0),
-      0
-    );
-    setTotalScore(score);
+    if (props.userAnswers && props.userAnswers.length > 0) {
+      const score = props.userAnswers.reduce(
+        (acc, value) => acc + (value ? 1 : 0),
+        0
+      );
+      setTotalScore(score);
 
-    const percentage = (score / maxScore) * 100;
-    const resultAnimation = animationForResult(percentage);
+      const maxScore = props.userAnswers.length;
+      const percentage = (score / maxScore) * 100;
+      const resultAnimation = animationForResult(percentage);
 
-    setMessage(resultAnimation);
+      setMessage(resultAnimation);
+    }
   }, [props.userAnswers]);
 
   return (
@@ -26,33 +27,31 @@ export function Results(props) {
         <h1
           className={`${styles.score} ${
             totalScore >= 80
-              ? styles.highScore
+              ? styles.heightScore
               : totalScore >= 50
               ? styles.mediumScore
               : styles.lowScore
-          }`}
+          }${totalScore >= 80 ? styles.monkey : ""}`}
         >
           Your results
         </h1>
-        <div
-          className={`${styles.messageContainer} ${
-            message.backgroundColor && styles[message.backgroundColor]
-          }`}
-          style={{ opacity: 1, backgroundColor: message.backgroundColor }}
-        >
-          <h2>{message.text}</h2>
+        <div className={`${styles.funText} ${styles.funnyAnimation}`}>
+          Having Fun with us!
         </div>
       </div>
-
-      <div className={styles.message}>
-        You scored {totalScore} out of {props.userAnswers.length}!
-      </div>
-      <button
-        className={styles.button}
-        onClick={() => window.location.reload()}
+      <div
+        className={`${styles.messageContainer}${
+          message.color && styles[message.color]
+        }`}
+        style={{ opacity: 0.8, color: message.color }}
       >
-        Retry 🦥🌝
-      </button>
+        <h2>{message.text}</h2>
+      </div>
+      <div className={styles.message}>
+        {props.userAnswers
+          ? `You scored ${totalScore} out of ${props.userAnswers.length}!`
+          : "No answers provided"}
+      </div>
     </>
   );
 }
@@ -61,17 +60,17 @@ function animationForResult(percentage) {
   if (percentage >= 80) {
     return {
       text: "Wow, you're a quiz master! Are you sure you haven't secretly been eating a dictionary for breakfast?",
-      backgroundColor: "green",
+      color: "green",
     };
   } else if (percentage >= 50) {
     return {
       text: "Well done! You're almost at the top. A little more practice, and you'll conquer the quiz crown!",
-      backgroundColor: "orange",
+      color: "orange",
     };
   } else {
     return {
       text: "Not bad! Remember: every master was once a beginner. On to the next try!",
-      backgroundColor: "red",
+      color: "red",
     };
   }
 }
