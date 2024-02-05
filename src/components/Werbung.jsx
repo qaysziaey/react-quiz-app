97437d23bc5248c8762e8c657c676fc77f200eca
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-02-05 10:43:27 +0100

    Move data-files from main branch

--- src/components/Werbung.jsx
+++ src/components/Werbung.jsx
@@ -1,11 +1,24 @@
-import './Werbung.module.css'
+import styles from "./Werbung.module.css";
 
-export function Werbung(props){
-    
-    return (
-        <>
-            <h1>Werbung</h1>
-        </>
-    )
+export function Werbung(props) {
+  return (
+    <>
+      <div className={styles["werbung-container"]}>
+        {/* <h1>Werbung</h1> */}
+        <p>Invite friends & win</p>
 
+        <span className={styles["money-emoji"]}>🤑</span>
+
+        <h2 className={styles["winn-prize"]}>50$</h2>
+        <div className={styles.stars}>
+          <span>💰</span>
+          <span>💰</span>
+          <span>💰</span>
+          <span>💰</span>
+          <span>💰</span>
+        </div>
+        <button className={styles.button}>Close Ad</button>
+      </div>
+    </>
+  );
 }
