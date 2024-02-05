d6e457170aae2102f0b6da83c018a1a83e1e812a
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-02-05 10:20:21 +0100

    Big Change for all Project

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,7 +2,7 @@ import styles from "./Question.module.css";
 import { data_of_questions } from "../data/data.js";
 import { useState } from "react";
 
-export function Question(props) {
+export function Question({ user, pull_of_questions, onChangePage }) {
   const [getData, setGetData] = useState(data_of_questions);
 
   //   function handleQuestions() {
@@ -15,7 +15,7 @@ export function Question(props) {
     <>
       <div className={styles["questions-container"]}>
         <h1>QUIZ QUESTIONS</h1>
-        <h2 className={styles["rounds-counter"]}>5/8</h2>
+        <h2 class="rounds-counter">5/8</h2>
         <div className={styles["questions"]}>
           <form>
             {data_of_questions.prizes_by_points.map((prize, index) => {})}
@@ -23,27 +23,29 @@ export function Question(props) {
               Wie selektiert man ein Element mit id=demo?
             </p>
             <label>
-              <input name="a" type="checkbox" value="option1" checked />
+              <input name="answer" type="radio" value="1" />
               #demo
             </label>
 
             <label>
-              <input name="b" type="checkbox" value="option1" />
+              <input name="answer" type="radio" value="2" />
               .demo
             </label>
             <label>
-              <input name="c" type="checkbox" value="option1" />
+              <input name="answer" type="radio" value="3" />
               Demo
             </label>
             <label>
-              <input name="d" type="checkbox" value="option1" />
+              <input name="answer" type="radio" value="4" />
               *demo
             </label>
           </form>
         </div>
-        <div className={styles["buttons-contanier"]}>
-          <button className={styles.button}>Back</button>
-          <button className={styles.button}>Next</button>
+        <div class="buttons-contanier">
+          <button class="button" onClick={() => {
+                    onChangePage(1);
+                }}>Back</button>
+          <button class="button">Next</button>
         </div>
       </div>
     </>
