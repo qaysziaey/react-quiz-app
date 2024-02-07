f293787f6eed45e026f13e5057d658b014a228ba
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-02-07 17:39:38 +0100

    some changes

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,24 +1,26 @@
 import styles from "./Question.module.css";
 import { data_of_questions } from "../data/data.js";
+import { languageList } from "../data/language.js";
 import { useState } from "react";
 
-export function Question({ user, onChangePage, onStartWithUser }) {
-  //   console.log(user)
+export function Question({ user, onChangePage, onStartWithUser, language }) {
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [feedback, setFeedback] = useState("");
   const [checkBoxState, setCheckBoxState] = useState([]);
 
   const handleNextQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setFeedback(""); // Reset feedback for the next question
   };
+
+
   const handlePreviousQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    setFeedback(""); // Reset feedback for the previous question
   };
 
-  let username = user.username;
-  let avatar = user.avatar;
-
-  // Derived state
+  const { username, avatar } = user;
 
   const answers = data_of_questions.questions.map((question, index) => {
     if (checkBoxState[index] === undefined) {
@@ -35,21 +37,35 @@ export function Question({ user, onChangePage, onStartWithUser }) {
     return acc + (cur >= 0 ? cur : 0);
   }, 0);
 
+  
+
   const onCheckAnswer = (value) => {
+
+    let tempCheckBoxState;
+
     if (currentQuestionIndex < checkBoxState.length) {
-      setCheckBoxState(
-        checkBoxState.map((state, index) => {
-          if (currentQuestionIndex === index) {
-            return Number(value);
-          } else {
-            return state;
-          }
-        })
-      );
+
+      console.log(checkBoxState.map((state, index) => {
+        if (currentQuestionIndex === index) {
+          return Number(value);
+        } else {
+          return state;
+        }
+      }));
+
     } else {
       setCheckBoxState([...checkBoxState, Number(value)]);
     }
 
+    
+
+/*
+    if(correct_){
+      setFeedback(languageList[language].Questions.text_correct);
+    }else{
+      setFeedback(languageList[language].Questions.text_incorrect);
+    }
+*/
     onStartWithUser({
       username,
       avatar,
@@ -62,81 +78,108 @@ export function Question({ user, onChangePage, onStartWithUser }) {
   const currentQuestion = data_of_questions.questions[currentQuestionIndex];
 
   if (currentQuestionIndex > data_of_questions.questions.length - 1) {
-    // TODO: in diese Platz machen users-add
-/*
+    let new_user = true;
+
+    
     onStartWithUser({
       username,
       avatar,
       answers,
       totalScore,
-    });    
-*/
+    });
+ 
+    
+    console.log(checkBoxState)
+    console.log(answers)
+
     onChangePage(3);
 
     return false;
 
   }
 
-  let temp_answers = currentQuestion.answers;
+  const temp_answers = currentQuestion.answers;
 
-  // Calculate derived state
-
-  // console.log(temp_user_answers)
+  console.log(checkBoxState)
 
   return (
-    <>
-      <div className={styles["questions-container"]}>
-        <h1>Questions</h1>
-        <h2 className={styles["rounds-counter"]}>
-          {currentQuestion.id}/{data_of_questions.questions.length}
-        </h2>
-        <div className={styles["questions"]}>
-          <h2>{currentQuestion.question.text}</h2>
-          <form>
-            {temp_answers.map((answer, key) => (
-              <label key={answer.number} className={styles["answer"]}>
-                <input
-                  name="forQuestion"
-                  type="radio"
-                  value={key}
-                  checked={
-                    checkBoxState[currentQuestionIndex] == key ? true : false
-                  }
-                  onChange={(event) => {
-                    onCheckAnswer(event.target.value);
-                  }}
-                  // Add onChange handler if you need to handle user selections
-                />
-                {answer.text}
-              </label>
-            ))}
-          </form>
-        </div>
-        <div className={styles["buttons-contanier"]}>
-          <button
-            className="button"
-            onClick={() => {
-              if (currentQuestionIndex <= 0) {
-                onChangePage(1);
-              } else {
-                handlePreviousQuestion();
+    <div className={styles["questions-container"]}>
+      <h1>{languageList[language].Questions.Title}</h1>
+      <h2 className={styles["rounds-counter"]}>
+        {currentQuestion.id}/{data_of_questions.questions.length}
+      </h2>
+      <div className={styles["questions"]}>
+        <h2>{currentQuestion.question.text[language]}</h2>
+        <form>
+          {temp_answers.map((answer, index) => (
+            <label key={index} className={  
+                styles[
+                    (
+                      checkBoxState[currentQuestionIndex]==undefined?'':
+                    (
+                      index==checkBoxState[currentQuestionIndex] && answer.correct? "enabled1" : "disabled1"
+                    ))
+                ] 
               }
-            }}
+              >
+              <input
+                name="forQuestion"
+                type="radio"
+                value={index}
+                checked={
+                  checkBoxState[currentQuestionIndex] == index ? true : false
+                }
+                onChange={(event) => {
+                  onCheckAnswer(event.target.value);
+                  if(answer.correct){
+                    setFeedback(languageList[language].Questions.text_correct);
+                  }else{
+                    setFeedback(languageList[language].Questions.text_incorrect);
+                  }
+                }}
+                disabled={
+                  checkBoxState[currentQuestionIndex] >= 0 &&
+                  checkBoxState[currentQuestionIndex] !== index
+                }
+              />
+              {answer.text[language]}
+            </label>
+          ))}
+        </form>
+        {feedback && (
+          <p
+            className={
+              feedback === languageList[language].Questions.text_correct ? styles["correct"] : styles["incorrect"]
+            }
           >
-            {currentQuestionIndex <= 0 ? "Main" : "Back"}
-          </button>
-          <button
-            className="button"
-            onClick={() => {
-              handleNextQuestion();
-            }}
-          >
-            {currentQuestionIndex >= data_of_questions.questions.length - 1
-              ? "Finish"
-              : "Next"}
-          </button>
-        </div>
+            {feedback}
+          </p>
+        )}
       </div>
-    </>
+      <div className={styles["buttons-container"]}>
+        <button
+          className="button"
+          onClick={() => {
+            if (currentQuestionIndex <= 0) {
+              onChangePage(1);
+            } else {
+              handlePreviousQuestion();
+            }
+          }}
+        >
+          {currentQuestionIndex <= 0 ? languageList[language].Questions.btn_main : languageList[language].Questions.btn_back}
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            handleNextQuestion();
+          }}
+        >
+          {currentQuestionIndex >= data_of_questions.questions.length - 1
+            ? languageList[language].Questions.btn_finish
+            : languageList[language].Questions.btn_next}
+        </button>
+      </div>
+    </div>
   );
 }
---
+++ src/data/language.js
@@ -0,0 +1,76 @@
+export const languageList = {
+    EN: {
+        'App':{
+            btn_change_lang:'🇬🇧',
+        },
+        'MainPage':{
+            'Title': "Your IQ under attack",
+            'btn_start': "START",
+            'label_avatar': "Your Avatar",
+            'label_name': "Your Name",
+            'btn_learn_more': "Learn more",
+            'alert_login': "Enter Your name and choose Avatar"
+        },
+        'Questions':{
+            'Title': "QUIZ QUESTIONS",
+            'btn_main': 'Main',
+            'btn_back': 'Back',
+            'btn_next': 'Next',
+            'btn_finish': 'Finish',
+            'text_incorrect': 'Nooooo!',
+            'text_correct': 'Super!',
+        },
+        'Werbung':{
+            'btn_close_ad': 'Close Ad'
+        },
+        'Results':{
+            'text_have_fun': 'Having Fun with us!',
+            'btn_play_again': 'Play again',
+            'text_no_answers': 'No answers provided',
+            'text_80':"Wow, you're a quiz master! Are you sure you haven't secretly been eating a dictionary for breakfast?",
+            'text_50':"Well done! You're almost at the top. A little more practice, and you'll conquer the quiz crown!",
+            'text_0':"Not bad! Remember: every master was once a beginner. On to the next try!",
+        },
+        'Learn_more':{
+            text1: "is a bester game of the world!!!",
+            text2: "And we make it!" 
+        }
+    },
+    DE: {
+        'App':{
+            btn_change_lang:'🇩🇪',
+        },
+        'MainPage':{
+            'Title': "Ihr IQ wird angegriffen",
+            'btn_start': "BEGINNEN",
+            'label_avatar': "Dein Avatar",
+            'label_name': "Dein Name",
+            'btn_learn_more': "Mehr erfahren",
+            'alert_login': "Geben Sie Ihren Namen ein und wählen Sie Avatar"
+        },
+        'Questions':{
+            'Title': "QUIZFRAGEN",
+            'btn_main': 'Main',
+            'btn_back': 'Zurück',
+            'btn_next': 'Weiter',
+            'btn_finish': 'Fertig',
+            'text_incorrect': 'Nooooo!',
+            'text_correct': 'Super!',
+        },
+        'Werbung':{
+            'btn_close_ad': 'Anzeige schließen'
+        },
+        'Results':{
+            'text_have_fun': 'Viel Spaß mit uns!',
+            'btn_play_again': 'Nochmal abspielen',
+            'text_no_answers': 'Keine Antworten gegeben',
+            'text_80':"Wow, du bist ein Quizmeister! Bist du sicher, dass du nicht heimlich ein Wörterbuch zum Frühstück gegessen hast?",
+            'text_50':"Gut gemacht! Du bist fast ganz oben. Noch ein bisschen Übung, dann eroberst du die Quiz-Krone!",
+            'text_0':"Nicht schlecht! Denken Sie daran: Jeder Meister war einmal ein Anfänger. Auf zum nächsten Versuch!",
+        },        
+        'Learn_more':{
+            text1: "ist das beste Spiel der Welt!!!",
+            text2: "Und wir schaffen es!"
+        }
+    },
+}
